882dcce812a2fedd8d27e4f72b47741871be6f63
Litiyajeyaraj <lidiyajeyaraj@gmail.com>
2019-11-08 14:43:26 +0530

    Added to branch Lidiya

--- src/story/dd.java
+++ src/story/dd.java
@@ -5,6 +5,8 @@ public class dd {
 	public static void main(String[] args) {
 		System.out.println("hgdfsag");
 		System.out.println("hgdfsag");
+		System.out.println("lidiya is updated");
+		System.out.println("program done successfull");
 	}
 
 }
